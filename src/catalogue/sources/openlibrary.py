93e1f3c229e3a29f996cd34104f1a22b37a28b3c
Maxime Visconte <maxime@visconte.fr>
2026-06-06 00:42:39 +0200

    fix dump dir

--- src/catalogue/sources/openlibrary.py
+++ src/catalogue/sources/openlibrary.py
@@ -18,7 +18,7 @@ from typing import Iterator
 logger = logging.getLogger(__name__)
 
 # Chemin vers les dumps
-DUMP_DIR = Path("/data")
+DUMP_DIR = Path("data")
 
 # Types de dump à utiliser
 WORKS_DUMP = "works.txt.gz"
